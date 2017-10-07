7caabf8305985347026d36ef5ec82b8a9e831f73
Kevin <kyrodriguez97@gmail.com>
2017-10-06 19:19:05 -0500

    Onclick envent

--- app/src/main/java/co/edu/uninpahu/calculadora/MainActivity.java
+++ app/src/main/java/co/edu/uninpahu/calculadora/MainActivity.java
@@ -11,18 +11,74 @@ import com.example.estudiantes.calculadora.R;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView screen;
     private Context mContext;
     private Integer numA , numB = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button zero = (Button) findViewById(R.id.btn0);
+        Button one = (Button) findViewById(R.id.btn1);
+        Button two = (Button) findViewById(R.id.btn2);
+        Button three = (Button) findViewById(R.id.btn3);
+        Button four = (Button) findViewById(R.id.btn4);
+        Button five = (Button) findViewById(R.id.btn5);
+        Button six = (Button) findViewById(R.id.btn6);
+        Button seven = (Button) findViewById(R.id.btn7);
+        Button eigth = (Button) findViewById(R.id.btn8);
+        Button nine = (Button) findViewById(R.id.btn9);
+        zero.setOnClickListener(this);
+        one.setOnClickListener(this);
+        two.setOnClickListener(this);
+        three.setOnClickListener(this);
+        four.setOnClickListener(this);
+        five.setOnClickListener(this);
+        six.setOnClickListener(this);
+        seven.setOnClickListener(this);
+        eigth.setOnClickListener(this);
+        nine.setOnClickListener(this);
     }
 
-
+    @Override
+    public void onClick(View button_var) {
+        int button = button_var.getId();
+        TextView result = (TextView)findViewById(R.id.resultView);
+        switch (button){
+            case R.id.btn0:
+                result.append("0");
+                break;
+            case R.id.btn1:
+                result.append("1");
+                break;
+            case R.id.btn2:
+                result.append("2");
+                break;
+            case R.id.btn3:
+                result.append("3");
+                break;
+            case R.id.btn4:
+                result.append("4");
+                break;
+            case R.id.btn5:
+                result.append("5");
+                break;
+            case R.id.btn6:
+                result.append("6");
+                break;
+            case R.id.btn7:
+                result.append("7");
+                break;
+            case R.id.btn8:
+                result.append("8");
+                break;
+            case R.id.btn9:
+                result.append("9");
+                break;
+            default:
+                break;
+        }
+    }
 }
